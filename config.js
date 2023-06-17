cd532a665e31645c81e84083c963eae5f1037b0c
wronglylightcig <136865859+wronglylightcig@users.noreply.github.com>
2023-06-17 08:29:43 +0700

    config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '64df23cc8ab0a64d3c2da6ba31e47808', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
